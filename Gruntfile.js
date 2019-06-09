ff1bd0aa968302447dcec80251af64ffcc2085c5
Yeremi <yeremiloli@yahoo.com>
2019-06-09 03:12:25 -0300

    Refactoring

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,25 +4,44 @@ module.exports = function (grunt) {
     // CONFIG
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        banner: {
+            compact: '/*! <%= pkg.name %> v<%= pkg.version %> https://github.com/yeremi Copyright (c) 2019 Yeremi Loli - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */ \n'
+        },
+        concat: {
+            options: {
+                separator: '\n\n\n',
+                stripBanners: true,
+                banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' + '<%= grunt.template.today("yyyy-mm-dd") %> */ \n\n'
+            },
+            'dist/xodo.js': ['src/sizzle.js', 'src/xodo.js']
+        },
         uglify: {
+            options: {
+                stripbanners: true,
+                banner: '<%= banner.compact %>',
+                beautify: {
+                    ascii_only: true
+                }
+            },
             dist: {
-                src: ['index.js'],
-                dest: 'src/xodo.min.js'
+                src: ['dist/xodo.js'],
+                dest: 'dist/xodo.min.js'
             },
             docs: {
-                src: ['index.js'],
-                dest: 'docs/xodo.min.js'
+                src: ['dist/xodo.js'],
+                dest: 'dist/xodo.min.js'
             }
         },
         jshint: {
-            all: ['index.js']
-        }
+            all: ['src/xodo.js']
+        },
     });
 
     // PLUGINS
-    grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
 
     // TASKS
-    grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };
